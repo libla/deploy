e1c737ad8a89f200a9717afcf191f13d1062cc6c
libla <libla@qq.com>
2015-12-28 06:19:00 +0800

    sh

--- node.js/LTS/build.sh
+++ node.js/LTS/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 git clone -b v4.2.4 https://github.com/nodejs/node.git node.js
 cd node.js
 ./configure
--- ssdb/Stable/build.sh
+++ ssdb/Stable/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-
+git clone -b stable-1.9.2 https://github.com/ideawu/ssdb.git ssdb
+cd ssdb
+make
+sudo make install
